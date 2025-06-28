4f03f66792944ff031ca8ca46619e7f1319d2ef0
Avontae Broomfield <avontaebroomfield@Avontaes-MacBook-Air.local>
2025-06-28 11:50:12 -0500

    add color change on click

--- piano.py
+++ piano.py
@@ -2,6 +2,7 @@ import pygame
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BLUE = (0, 150, 255)
 
 class Piano:
     def __init__(self):
@@ -20,32 +21,59 @@ class Piano:
 
         # Create white keys
         for i in range(14):  # C to B
-            rect = pygame.Rect  ((i * white_key_width)+100, 350, white_key_width, white_key_height)
+            rect = pygame.Rect((i * white_key_width) + 100, 350, white_key_width, white_key_height)
             self.white_keys.append(rect)
 
         # Create black keys (skip E and B)
-        skip = [2, 6, 9, 13]  # No black keys after E (index 2) and B (index 6)
-        for i in range(14):  # 5 black keys total
+        skip = [2, 6, 9, 13]  # Indices after which there are no black keys
+        for i in range(14):
             if i not in skip:
-                rect = pygame.Rect(     ((i + 1) * white_key_width - black_key_width // 2) + 100 , 350, black_key_width, black_key_height)
+                rect = pygame.Rect(((i + 1) * white_key_width - black_key_width // 2) + 100, 350, black_key_width, black_key_height)
                 self.black_keys.append(rect)
 
-    def draw_keys(self):
-        self.screen.fill((0, 0, 0))  # Background black
-        for rect in self.white_keys:
-            pygame.draw.rect(self.screen, WHITE, rect)
-            pygame.draw.rect(self.screen, BLACK, rect, 2)  # Outline
+    def draw_keys(self, pressed_white, pressed_black):
+        self.screen.fill(BLACK)
 
-        for rect in self.black_keys:
-            pygame.draw.rect(self.screen, BLACK, rect)
+        for i, rect in enumerate(self.white_keys):
+            color = BLUE if i == pressed_white else WHITE
+            pygame.draw.rect(self.screen, color, rect)
+            pygame.draw.rect(self.screen, BLACK, rect, 2)
+
+        for i, rect in enumerate(self.black_keys):
+            color = BLUE if i == pressed_black else BLACK
+            pygame.draw.rect(self.screen, color, rect)
 
     def play(self):
         running = True
+        pressed_white = -1
+        pressed_black = -1
+
         while running:
-            self.draw_keys()
+            self.draw_keys(pressed_white, pressed_black)
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                     pygame.quit()
+
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    pressed_white = -1
+                    pressed_black = -1
+
+                    # Check black keys first (they sit on top visually)
+                    for i, rect in enumerate(self.black_keys):
+                        if rect.collidepoint(event.pos):
+                            pressed_black = i
+                            break
+
+                    # If no black key was clicked, check white keys
+                    if pressed_black == -1:
+                        for i, rect in enumerate(self.white_keys):
+                            if rect.collidepoint(event.pos):
+                                pressed_white = i
+                                break
+
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    pressed_white = -1
+                    pressed_black = -1
